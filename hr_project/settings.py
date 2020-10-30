b0f8b48cde46622a2a5a407202fcadcb2ba54392
roshak7 <roshak7@gmail.com>
2020-10-30 23:47:41 +0300

    last

--- hr_project/settings.py
+++ hr_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','5+jrdw6kpzjb266srzn0_&yl$t8*-0r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','0.0.0.0','sleepy-woodland-58553.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','0.0.0.0']
 
 
 # Application definition
